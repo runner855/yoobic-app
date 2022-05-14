fc6989ce73a57fd604e33a63b00e417323006622
Dario Milani <dariomilani855@icloud.com>
2022-05-14 19:36:55 +0200

    learning angular components

--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import { MycomponentComponent } from './mycomponent/mycomponent.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    MycomponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
