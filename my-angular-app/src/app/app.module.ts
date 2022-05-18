0a55c9a62e5993fe953fb05ecdb5915b85169cdc
Dario Milani <dariomilani855@icloud.com>
2022-05-18 09:39:28 +0200

    app set up

--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -1,17 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
-import { MycomponentComponent } from './mycomponent/mycomponent.component';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    MycomponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
