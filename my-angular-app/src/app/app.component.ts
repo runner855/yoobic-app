570057b637a12f63df6a51d4db4a7701bcb769bb
Dario Milani <dariomilani855@icloud.com>
2022-05-19 00:00:29 +0200

    data bindings and events

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { reduceEachLeadingCommentRange } from 'typescript';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+
 })
 export class AppComponent {
-  name = 'Dario';
+
 
 }
--- my-angular-app/src/app/app.module.ts
+++ my-angular-app/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServerComponent } from './server/server.component';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    MainComponent
+
   ],
   imports: [
     BrowserModule,
