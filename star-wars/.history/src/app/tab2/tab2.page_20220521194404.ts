135c28e0b513e1fc55d40a7b3be1ed2f2d63379b
Dario Milani <dariomilani855@icloud.com>
2022-05-22 16:35:30 +0200

    workin on routing

---
+++ star-wars/.history/src/app/tab2/tab2.page_20220521194404.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor(private menu: MenuController) { }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+
+  }
