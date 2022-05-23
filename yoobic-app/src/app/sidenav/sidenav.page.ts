54f0e273eac185bba8ac39c067c41b5823a701f8
Dario Milani <dariomilani855@icloud.com>
2022-05-23 10:35:54 +0200

    app set up

--- yoobic-app/src/app/sidenav/sidenav.page.ts
+++ yoobic-app/src/app/sidenav/sidenav.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, RouterEvent } from '@angular/router';
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.page.html',
@@ -7,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavPage implements OnInit {
 
-  constructor() { }
+  active = '';
+
+  NAV = [
+    {
+      name:'Login',
+      link: '/nav/login',
+      icon: 'image-sharp'
+    },
+    {
+      name: 'Heroes',
+      link: '/nav/heroes',
+      icon: 'image-sharp'
+    },
+    {
+      name: 'Hero',
+      link: '/nav/hero',
+      icon: 'image-sharp'
+    }
+  ]
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.active = event.url
+    })
+   }
 
   ngOnInit() {
   }
