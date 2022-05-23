f4f82d17287e50e62c014416bf41431442d25073
Dario Milani <dariomilani855@icloud.com>
2022-05-23 12:37:33 +0200

    on click navigate to another page

--- yoobic-app/src/app/pages/login/login.page.ts
+++ yoobic-app/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
+
+  onLogin(form: NgForm) {
+    this.router.navigateByUrl('/heroes')
+  }
 
   ngOnInit() {
   }
