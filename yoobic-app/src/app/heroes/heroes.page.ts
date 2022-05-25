dc36ca6c533aaaf9885f30b0e9675d3468c29f11
Dario Milani <dariomilani855@icloud.com>
2022-05-25 10:27:15 +0200

    rendering card details on click , now needs to be rendered in different page

--- yoobic-app/src/app/heroes/heroes.page.ts
+++ yoobic-app/src/app/heroes/heroes.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HEROES } from '../heroes';
+import { Hero } from '../hero';
 
 
 @Component({
@@ -12,16 +13,22 @@ import { HEROES } from '../heroes';
 export class HeroesPage implements OnInit {
 
   heroes = HEROES;
-  
+  selectedHero?: Hero;
+ 
 
   
 
 
   constructor(public router : Router) { }
 
-  onLearnMore(form: NgForm) {
-    this.router.navigateByUrl('/hero')
+  // onLearnMore(form: NgForm) {
+  //   this.router.navigateByUrl('/hero')
+  // }
+
+  onSelect(hero:Hero): void {
+    this.selectedHero = hero;
   }
+  
 
   ngOnInit() {
   }
